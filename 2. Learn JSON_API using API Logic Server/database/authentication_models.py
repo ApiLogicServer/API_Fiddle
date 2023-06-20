325ac302b42f065f1867c3e6d5f6c2a1e6566ab2
Val Huber <valjhuber@gmail.com>
2023-06-19 19:30:19 -0700

    08.04.30: nw- fix

--- 2. Learn JSON_API using API Logic Server/database/authentication_models.py
+++ 2. Learn JSON_API using API Logic Server/database/authentication_models.py
@@ -15,7 +15,7 @@ from flask_jwt_extended import create_access_token
 # Alter this file per your database maintenance policy
 #    See https://apilogicserver.github.io/Docs/Project-Rebuild/#rebuilding
 #
-# Created:  June 19, 2023 08:10:31
+# Created:  June 19, 2023 19:27:25
 # Database: sqlite:////Users/val/dev/Org-ApiLogicServer/API_Fiddle/3. Logic/database/authentication_db.sqlite
 # Dialect:  sqlite
 #
